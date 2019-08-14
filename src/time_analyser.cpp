edea515b19fb4d501603cc273be7a71065d9e98e
DarlanJurak <darlan.jurak@gmail.com>
2019-08-14 15:23:42 -0300

    add stop flag

--- src/time_analyser.cpp
+++ src/time_analyser.cpp
@@ -7,59 +7,60 @@
 #include <sstream>
 #include <fstream>
 
+// Hash to store msg and publishing time
 std::unordered_map<std::string, ros::Time> umap; 
 
-ros::Time begin;
-int counter = 0;
+// Time measurement
+ros::Time     begin;
 ros::Duration spent_time;
 
+// File writing
 std::ofstream myfile("/home/adomingues/gaph/darlan/dataCollection_1byte.txt", std::ios::out | std::ios::binary);
 
+// Callback executed when mpsoc publish into /mpsoc_to_ros topic
+// Here we measure the travel time of the sent message
 void mpsocToRosCallback(const std_msgs::String::ConstPtr& msg)
 {
   spent_time = ros::Time::now() - begin;
-  // ROS_INFO("Spent time: %d: [%d nano seconds]", counter, spent_time.toNSec());
-
   std::unordered_map<std::string, ros::Time>::const_iterator got = umap.find(msg->data);
-  std::cout << "Recebeu: "<< got->first << " ---- demorou: " << spent_time.toNSec() << "nsecs" << std::endl;
-
-  myfile << got->first << "," << spent_time.toNSec() << std::endl;
-  
-  counter++;
+  myfile << got->first << ", " << spent_time.toNSec() << std::endl;
 }
 
 int main(int argc, char **argv)
 {
 
+  // ROS node handler and initializer
   ros::init(argc, argv, "time_analyser");
   ros::NodeHandle n;
 
+  // Parametrizable publishing rate
   double rate;
   n.param("/time_analyser_rate", rate, 2.0);
 
+  // test message publisher 
   ros::Publisher orca_ros_to_mpsoc_pub = n.advertise<std_msgs::String>("orca_ros_to_mpsoc", 1000);
   ros::Rate loop_rate(rate);
 
+  // mpsoc publishing listener
   ros::Subscriber orca_mpsoc_to_ros_pub = n.subscribe("orca_mpsoc_to_ros", 1000, mpsocToRosCallback);
 
   uint8_t count = 0;
-  while (ros::ok())
+  while (ros::ok() && (count < 100))
   {
     // msg to be written
     std_msgs::String  msg;
     std::stringstream ss;
 
-    // msg size control
+    // msg's size control
     for (int i = 0; i < 1; i++)
     {
       ss << std::to_string(count);
     }
 
     msg.data = ss.str();
-    // ROS_INFO("data: %d, data: %s, sizeof: %d", count, msg.data.c_str(), msg.data.size());
-
     orca_ros_to_mpsoc_pub.publish(msg);
 
+    // publishing time 
     begin = ros::Time::now();
     umap[msg.data] = begin;
 
@@ -70,13 +71,6 @@ int main(int argc, char **argv)
 
     n.getParam("/time_analyser_rate", rate);
     loop_rate = ros::Rate(rate);
-
-    // if (count == 30) {
-    //   for (auto x : umap) 
-    //     std::cout << x.first.c_str() << " " << x.second << std::endl;
-    //   std::unordered_map<std::string, ros::Time>::const_iterator got = umap.find(std::to_string(count - 1));
-    //   std::cout << "Publicou: "<< got->first << " ---- no tempo: " << got->second << std::endl;
-    // }
       
   }
 
